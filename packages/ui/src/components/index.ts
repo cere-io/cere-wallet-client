6cd695710a3fac146159053d57343bb82a3079d1
Sergey Kambalin <sergey.kambalin@cere.io>
2022-08-03 11:04:33 +0600

    Wallet UI Kit minimal setup

---
+++ packages/ui/src/components/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Export only required set of components and utils from MUI, so we know what is used from the library and what is not
+ */
+
+export { Button, type ButtonProps } from "@mui/material";
