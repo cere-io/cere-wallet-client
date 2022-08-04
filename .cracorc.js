248936e0ac73cf18fe12427b19e78a1b624dff03
Sergey Kambalin <sergey.kambalin@cere.io>
2022-08-04 13:23:40 +0600

    Refactor build configuration a bit

--- .cracorc.js
+++ .cracorc.js
@@ -1,5 +1,5 @@
-const { inspect } = require("util");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const { getLoader, loaderByName } = require('@craco/craco');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   webpack: {
@@ -7,14 +7,16 @@ module.exports = {
       config.resolve.plugins = [new TsconfigPathsPlugin()];
 
       /**
-       * TODO: Figure out a better approach of compiling pacakges outside of `src` (eg. by using project references)
+       * TODO: Figure out a better approach of compiling packages outside of `src` (eg. by using project references)
        *
        * Let Babel compile outside of `src`
        */
-      const oneOfRule = config.module.rules.find((rule) => rule.oneOf);
-      const tsRule = oneOfRule.oneOf.find((rule) => rule.test.toString().includes("ts|tsx"));
-      tsRule.include = undefined;
-      tsRule.exclude = /node_modules/;
+      const { match } = getLoader(config, loaderByName('babel-loader'));
+
+      Object.assign(match.loader, {
+        include: undefined,
+        exclude: /node_modules/,
+      });
 
       return config;
     },
