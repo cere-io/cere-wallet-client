d5fd29b1c65665cd49804a044168dbe8256153d2
Sergey Kambalin <sergey.kambalin@cere.io>
2022-08-08 17:15:51 +0600

    [DAV-2801] Add comment for code reviewers

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
 import { App } from './App';
-window.process = process;
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
