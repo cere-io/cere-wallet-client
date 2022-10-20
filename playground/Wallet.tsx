a82263c11ac562b1af55b3fe60dde5a5acce16d9
Sergey Kambalin <sergey.kambalin@cere.io>
2022-10-20 16:50:27 +0600

    [DAV-3519] Add new methods to SDK (#37)

--- playground/Wallet.tsx
+++ playground/Wallet.tsx
@@ -19,6 +19,12 @@ export const Wallet = () => {
     wallet.disconnect();
   }, [wallet]);
 
+  const handleUserInfo = useCallback(async () => {
+    const info = await wallet.getUserInfo();
+
+    console.log('User Info', info);
+  }, [wallet]);
+
   const handleGetAddress = useCallback(async () => {
     const provider = new providers.Web3Provider(wallet.provider);
     const signer = provider.getSigner();
@@ -28,12 +34,24 @@ export const Wallet = () => {
     alert(`Address: ${address}`);
   }, [wallet]);
 
+  const handleShowWallet = useCallback(async () => {
+    wallet.showWallet();
+  }, [wallet]);
+
   return (
     <Stack alignItems="center" spacing={2} paddingY={5}>
       {status === 'connected' || status === 'disconnecting' ? (
         <>
           <Button variant="outlined" color="primary" onClick={handleGetAddress}>
             Get Address
+          </Button>
+
+          <Button variant="outlined" color="primary" disabled={status === 'disconnecting'} onClick={handleUserInfo}>
+            Get User Info
+          </Button>
+
+          <Button variant="outlined" color="primary" disabled={status === 'disconnecting'} onClick={handleShowWallet}>
+            Show wallet
           </Button>
 
           <Button variant="contained" color="primary" disabled={status === 'disconnecting'} onClick={handleDisconnect}>
