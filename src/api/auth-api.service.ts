53f6f9d4d077cd1aa976f22b59eaea588755fd14
Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>
2022-10-24 13:03:24 +0300

    feat(auth) social auth (DAV-3517)
     - added socal auth via Google and Facebook

--- src/api/auth-api.service.ts
+++ src/api/auth-api.service.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
+import { WALLET_API } from '~/constants';
 
 const api = axios.create({
-  baseURL: 'https://api.wallet.dev.cere.io',
+  baseURL: WALLET_API,
 });
 
 export class AuthApiService {
@@ -15,10 +16,25 @@ export class AuthApiService {
     return result?.data?.code === 'SUCCESS';
   }
 
-  public static async getToken(email: string, code: string): Promise<string | null> {
+  public static async getTokenByEmail(email: string, code: string): Promise<string | null> {
     let result: AxiosResponse<{ code: 'SUCCESS' | 'ERROR'; data: { token: string } }> | null = null;
     try {
-      result = await api.post<{ code: 'SUCCESS' | 'ERROR'; data: { token: string } }>('/auth/token', { email, code });
+      result = await api.post<{ code: 'SUCCESS' | 'ERROR'; data: { token: string } }>('/auth/token-by-email', {
+        email,
+        code,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    return result?.data.code === 'SUCCESS' ? result?.data.data.token : null;
+  }
+
+  public static async getTokenBySocial(socialToken: string): Promise<string | null> {
+    let result: AxiosResponse<{ code: 'SUCCESS' | 'ERROR'; data: { token: string } }> | null = null;
+    try {
+      result = await api.post<{ code: 'SUCCESS' | 'ERROR'; data: { token: string } }>('/auth/token-by-social', {
+        token: socialToken,
+      });
     } catch (err) {
       console.error(err);
     }
