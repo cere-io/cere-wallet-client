08a75d090a67b73bb0dabfb59b4f11354308c59a
aleksey-svetliskiy-cere <90830691+aleksey-svetliskiy-cere@users.noreply.github.com>
2022-11-28 10:59:49 -0300

    feat(ramp) set correct email and asset (DAV-3708) (#70)
    Co-authored-by: Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>

--- src/hooks/useRamp.ts
+++ src/hooks/useRamp.ts
@@ -5,23 +5,27 @@ import { REACT_APP_RAMP_API_KEY } from '~/constants';
 
 interface RampProps {
   address: string;
+  email?: string;
 }
 
-export const useRamp = ({ address }: RampProps) => {
+export const useRamp = ({ address, email }: RampProps) => {
   const startPayment = useCallback(() => {
     const config: IHostConfig = {
       hostAppName: 'Cere Wallet',
       hostLogoUrl: `${window.origin}/images/logo.svg`,
-      defaultAsset: 'MATIC',
+      userEmailAddress: email,
       userAddress: address,
       variant: 'hosted-auto',
       hostApiKey: REACT_APP_RAMP_API_KEY,
     };
-    if (process.env.REACT_APP_ENV !== 'prod') {
+    if (process.env.REACT_APP_ENV === 'prod') {
+      config.defaultAsset = 'ETH_USDC';
+    } else {
+      config.defaultAsset = 'MATIC_USDC';
       config.url = 'https://ri-widget-staging.firebaseapp.com/';
     }
     new RampInstantSDK(config).show();
-  }, [address]);
+  }, [address, email]);
 
   return { startPayment };
 };
