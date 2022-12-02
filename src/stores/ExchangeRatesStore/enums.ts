2f4d0279f9b148537362ce68ccc55023f42fc7f6
kirilllogrus <k.zamotaev@gateline.ru>
2022-12-02 13:49:36 +0300

    DAV-3359 updated enums

--- src/stores/ExchangeRatesStore/enums.ts
+++ src/stores/ExchangeRatesStore/enums.ts
@@ -4,14 +4,15 @@ export const MATIC_ID = 'matic-network';
 export const MUMBAI = 'mumbai';
 export const MAINNET = 'mainnet';
 export const USD = 'usd';
+
 export const USDC = 'usdc';
 export const USDC_ID = 'usd-coin';
 
 export const MUMBAI_CHAIN_ID = '0x13881';
 
 export const TOKENS = [
-  { id: 'matic-network', name: MATIC },
-  { id: 'usd-coin', name: USDC },
+  { id: MATIC_ID, name: MATIC },
+  { id: USDC_ID, name: USDC },
 ];
 
 export const COINGECKO_PLATFORMS_CHAIN_CODE_MAP: Record<string, { platform: string; currency: string }> = {
