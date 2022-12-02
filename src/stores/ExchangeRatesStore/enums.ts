9994d1dd853f413e8bf96bf9af5ffefb45e741d4
kirilllogrus <k.zamotaev@gateline.ru>
2022-12-02 12:19:31 +0300

    DAV-3359 added usdc

--- src/stores/ExchangeRatesStore/enums.ts
+++ src/stores/ExchangeRatesStore/enums.ts
@@ -4,10 +4,14 @@ export const MATIC_ID = 'matic';
 export const MUMBAI = 'mumbai';
 export const MAINNET = 'mainnet';
 export const USD = 'usd';
+export const USDC = 'usdc';
 
 export const MUMBAI_CHAIN_ID = '0x13881';
 
-export const TOKENS = [{ id: 'matic-network', name: MATIC }];
+export const TOKENS = [
+  { id: 'matic-network', name: MATIC },
+  { id: 'orce-bridge-usdc', name: USDC },
+];
 
 export const COINGECKO_PLATFORMS_CHAIN_CODE_MAP: Record<string, { platform: string; currency: string }> = {
   [MUMBAI_CHAIN_ID]: {
