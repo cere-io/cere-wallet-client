cc6e283ea7e2a8643dc08739f1ddfc3acb10f485
kirilllogrus <k.zamotaev@gateline.ru>
2022-12-02 12:08:24 +0300

    DAV-3359 removed redundant const

--- src/stores/ExchangeRatesStore/enums.ts
+++ src/stores/ExchangeRatesStore/enums.ts
@@ -1,21 +1,10 @@
 export const ETH = 'eth';
 export const MATIC = 'matic';
+export const MATIC_ID = 'matic';
 export const MUMBAI = 'mumbai';
 export const MAINNET = 'mainnet';
 export const USD = 'usd';
 
-export const MAINNET_CODE = 1;
-export const GOERLI_CODE = 5;
-export const SEPOLIA_CODE = 11_155_111;
-export const MATIC_CODE = 137;
-export const MUMBAI_CODE = 80_001;
-
-export const MAINNET_CHAIN_ID = '0x1';
-export const ROPSTEN_CHAIN_ID = '0x3';
-export const RINKEBY_CHAIN_ID = '0x4';
-export const KOVAN_CHAIN_ID = '0x2a';
-export const GOERLI_CHAIN_ID = '0x5';
-export const MATIC_CHAIN_ID = '0x89';
 export const MUMBAI_CHAIN_ID = '0x13881';
 
 export const tokens = [{ id: 'matic-network', name: MATIC }];
@@ -23,7 +12,7 @@ export const tokens = [{ id: 'matic-network', name: MATIC }];
 export const COINGECKO_PLATFORMS_CHAIN_CODE_MAP: Record<string, { platform: string; currency: string }> = {
   [MUMBAI_CHAIN_ID]: {
     platform: 'polygon-pos',
-    currency: 'matic',
+    currency: MATIC,
   },
 };
 
