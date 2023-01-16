abf26671d9bca63daba1c3df831c14a3b9eebdb4
kirilllogrus <k.zamotaev@gateline.ru>
2023-01-16 13:17:19 +0300

    DAV-3719 fix select for hook form

--- src/components/AssetList/SelectNetwork.tsx
+++ src/components/AssetList/SelectNetwork.tsx
@@ -1,20 +1,12 @@
 import { FC } from 'react';
-import { MenuItem, Select } from '@cere-wallet/ui';
+import { MenuItem, Select, SelectProps } from '@cere-wallet/ui';
 import { MATIC_PLATFORMS } from '~/stores';
-
-interface SelectNetworkProps {
-  onChange: (value: string) => void;
-}
 
 const networkList = [{ value: MATIC_PLATFORMS.POLIGON, name: 'Polygon' }];
 
-export const SelectNetwork: FC<SelectNetworkProps> = ({ onChange }) => {
-  const handleChange = (item: Record<string, any>) => {
-    onChange(item.value);
-  };
-
+export const SelectNetwork: FC<SelectProps> = ({ onChange }) => {
   return (
-    <Select fullWidth placeholder="network" size="small" defaultValue={MATIC_PLATFORMS.POLIGON} onChange={handleChange}>
+    <Select fullWidth placeholder="network" size="small" defaultValue={MATIC_PLATFORMS.POLIGON} onChange={onChange}>
       {networkList.map((item) => (
         <MenuItem key={item.name} value={item.value}>
           {item.name}
