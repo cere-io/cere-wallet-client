ab2ccee288c38d76cd14340b339f2a354b9a8f6a
kirilllogrus <k.zamotaev@gateline.ru>
2023-01-17 15:22:52 +0300

    DAV-3719 fix rates collection

--- src/hooks/usePopularAssets.ts
+++ src/hooks/usePopularAssets.ts
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { COIN_GECKO_API } from '~/constants';
 import { Asset, NETWORKS_LIST } from '~/stores';
 
+const EXCLUDE_POPULAR_LIST = ['matic', 'usdc', 'cere'];
 const [ETHERIUM] = NETWORKS_LIST;
 const TOKEN_QUANTITY = 100;
 const COIN_GECKO_API_SEARCH = `${COIN_GECKO_API}/coins/markets?vs_currency=usd&category=ethereum-ecosystem&order=market_cap_desc&per_page=${TOKEN_QUANTITY}&page=1&sparkline=false`;
@@ -16,16 +17,20 @@ export function usePopularAssets() {
       const response = await fetch(`${COIN_GECKO_API_SEARCH}`);
       const data = await response.json();
 
-      const items: Asset[] = data.map((item: Record<string, string>) => ({
-        id: item.id,
-        ticker: item.symbol || '',
-        displayName: item.symbol?.toLocaleUpperCase(),
-        network: ETHERIUM.value,
-        type: 'ERC20',
-        thumb: item.image,
-        symbol: item.symbol,
-        decimals: item.symbol,
-      }));
+      const items: Asset[] = data.reduce((acc: Asset[], item: Record<string, string>) => {
+        if (!EXCLUDE_POPULAR_LIST.includes(item.symbol?.toLowerCase()))
+          acc.push({
+            id: item.id,
+            ticker: item.symbol || '',
+            displayName: item.symbol?.toLocaleUpperCase(),
+            network: ETHERIUM.value,
+            type: 'ERC20',
+            thumb: item.image,
+            decimals: 0,
+            balance: 0,
+          });
+        return acc;
+      }, []);
 
       setData(items);
     } catch (err) {
