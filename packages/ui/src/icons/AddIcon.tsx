e652e9f7f8e0354cc605a1f5c1dd38a819d3adfc
kirilllogrus <k.zamotaev@gateline.ru>
2023-01-27 09:32:58 +0300

    DAV-3491 fix review issue

--- packages/ui/src/icons/AddIcon.tsx
+++ packages/ui/src/icons/AddIcon.tsx
@@ -3,7 +3,7 @@ import { SvgIcon, SvgIconProps } from '@mui/material';
 
 export const AddIcon = memo((props: SvgIconProps) => (
   <SvgIcon {...props} viewBox="0 0 21 20">
-    <line x1="10.5" y1="3" x2="10.5" y2="17" stroke="#733BF5" stroke-width="2" stroke-linecap="round" />
-    <line x1="3.5" y1="10" x2="17.5" y2="10" stroke="#733BF5" stroke-width="2" stroke-linecap="round" />
+    <line x1="10.5" y1="3" x2="10.5" y2="17" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+    <line x1="3.5" y1="10" x2="17.5" y2="10" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
   </SvgIcon>
 ));
