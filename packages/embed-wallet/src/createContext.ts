8d3c39ffbd9ea2d7429e4b20bcc176c13e836d01
Sergey Kambalin <sergey.kambalin@cere.io>
2023-03-21 13:04:49 +0600

    Bundle analyzer + small bug fix

--- packages/embed-wallet/src/createContext.ts
+++ packages/embed-wallet/src/createContext.ts
@@ -1,11 +1,13 @@
 import { Context, PartialContext } from './types';
 
-export const createContext = (...contexts: (PartialContext | null | undefined)[]): Context => {
-  const defaultContext: Context = {
-    app: {
-      url: window.location.origin,
-    },
-  };
+export const createContext = (...contexts: (PartialContext | null | undefined)[]) => {
+  const context: PartialContext = Object.assign({}, ...contexts.filter(Boolean));
 
-  return Object.assign({}, defaultContext, ...contexts.filter(Boolean));
+  /**
+   * Apply defaults
+   */
+  context.app ||= {};
+  context.app.url ||= window.location.href;
+
+  return context as Context;
 };
