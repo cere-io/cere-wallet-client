e5e1985e0bd31467fb568d8539f89591f2f469cc
Sergey Kambalin <sergey.kambalin@cere.io>
2023-08-21 11:52:59 +0600

    Fix wdio configuration for NodeJs 18+

--- tests/wdio/config/ci.ts
+++ tests/wdio/config/ci.ts
@@ -1,10 +1,19 @@
+import dns from 'dns';
 import { options } from './options';
 
 export const createCIConfig = (baseConfig: WebdriverIO.Config): WebdriverIO.Config => ({
   ...baseConfig,
 
   baseUrl: options.targetUrl || 'http://host.docker.internal:4567/',
-  hostname: '127.0.0.1',
+  hostname: 'localhost',
   port: 4444,
   path: '/wd/hub/',
+
+  /**
+   * Fix for braking DNS lookup changes in NodeJS 18+
+   * https://github.com/webdriverio/webdriverio/issues/8279
+   */
+  beforeSession: () => {
+    dns.setDefaultResultOrder('ipv4first');
+  },
 });
