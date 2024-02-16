ba15b844023340a4c9017b3d89731b8055970e75
Sergey Kambalin <sergey.kambalin@cere.io>
2024-02-16 13:04:49 +0600

    Migrate from deprecated customauth library

--- src/stores/Web3AuthStore/getUserInfo.ts
+++ src/stores/Web3AuthStore/getUserInfo.ts
@@ -1,8 +1,15 @@
 import jwtDecode from 'jwt-decode';
-import type { Auth0UserInfo } from '@toruslabs/customauth';
 import type { UserInfo } from '@cere-wallet/communication';
 
 import { OPEN_LOGIN_VERIFIER } from '~/constants';
+
+interface Auth0UserInfo {
+  picture: string;
+  email: string;
+  name: string;
+  sub: string;
+  nickname: string;
+}
 
 export const getUserInfo = (idToken: string): UserInfo => {
   const { name, email, picture } = jwtDecode<Auth0UserInfo>(idToken);
