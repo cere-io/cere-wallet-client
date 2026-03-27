741004e2d055a7283d3859d6bac9b3d03e3c677a
gharibyan <khachatur.gharibyan@gmail.com>
2026-03-27 16:55:13 +0400

    fix crhomedriver config.

--- tests/wdio/config/local.ts
+++ tests/wdio/config/local.ts
@@ -1,5 +1,13 @@
 export const createLocalConfig = (baseConfig: WebdriverIO.Config): WebdriverIO.Config => ({
   ...baseConfig,
 
-  services: [...baseConfig.services, 'chromedriver'],
+  services: [
+    ...baseConfig.services,
+    [
+      'chromedriver',
+      {
+        chromedriverCustomPath: require.resolve('chromedriver/bin/chromedriver'),
+      },
+    ],
+  ],
 });
